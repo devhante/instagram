cd159117d6a905e760c938f36a3c05db63df87bd
kyungwook <ironside2001@naver.com>
2019-02-12 10:53:53 +0900

    [Add] react-navigation 적용용

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import AppContainer from './src/Navigator';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <AppContainer />
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/Navigator.tsx
@@ -0,0 +1,16 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+
+const RootSwitch = createSwitchNavigator(
+    {
+        Home: HomeScreen
+    },
+    {
+        initialRouteName: 'Home'
+    }
+);
+
+const AppContainer = createAppContainer(RootSwitch);
+
+
+export default AppContainer;
