93dcf964ca1e01c31d30d8af36320d6308c05ee8
kyungwook <ironside2001@naver.com>
2019-02-13 18:07:10 +0900

    [Add] Post 버튼, 본문 추가

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { PostImage } from './PostImage';
 
 export class Post extends Component {
@@ -13,10 +13,38 @@ export class Post extends Component {
                         source={require('../../assets/images/profile.png')}
                     />
                     <Text style={styles.name}>kyung_wook_</Text>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.menu}
+                            source={require('../../assets/icons/post/menu.png')}
+                        />
+                    </TouchableOpacity>
                 </View>
                 <PostImage />
 
-                <View style={styles.bottom} />
+                <View style={styles.bottom}>
+                    <TouchableOpacity>
+                        <Image
+                            style={[styles.bottomIcon, styles.favorIcon]}
+                            source={require('../../assets/icons/post/favor-outline.png')}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.bottomIcon}
+                            source={require('../../assets/icons/post/comment.png')}
+                        />
+                    </TouchableOpacity>
+
+                    <View style={styles.bottomSpace} />
+                    <TouchableOpacity>
+                        <Image
+                            style={styles.bottomIcon}
+                            source={require('../../assets/icons/post/collection-outline.png')}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.content}>kyung_wook_ 본문입니다.</Text>
             </View>
         );
     }
@@ -37,7 +65,31 @@ const styles = StyleSheet.create({
         margin: 12
     },
     name: {
+        flex: 1,
         fontWeight: 'bold'
     },
-    bottom: {}
+    menu: {
+        width: 24,
+        height: 24,
+        margin: 12
+    },
+    bottom: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 8
+    },
+    bottomIcon: {
+        width: 36,
+        height: 36
+    },
+    bottomSpace: {
+        flex: 1
+    },
+    favorIcon: {
+        marginRight: 8
+    },
+    content: {
+        marginLeft: 14
+    }
 });
